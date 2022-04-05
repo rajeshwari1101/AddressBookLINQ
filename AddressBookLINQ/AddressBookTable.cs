96e29e639a0c2e90f49bfbe03815ca276bdc48f9
rajeshwari1101 <rajeshwaripatil1101@gmail.com>
2022-04-05 08:36:16 +0530

    Creating Table

---
+++ AddressBookLINQ/AddressBookTable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLINQ
+{
+    internal class AddressBookTable
+    {
+        //Creating method to add data to table
+        public void AddToDataTable()
+        {
+            //Creating object of class
+            DataTable table = new DataTable();
+            table.Columns.Add("Id");
+            table.Columns.Add("FirstName");
+            table.Columns.Add("LastName");
+            table.Columns.Add("Address");
+            table.Columns.Add("City");
+            table.Columns.Add("State");
+            table.Columns.Add("Zip");
+            table.Columns.Add("PhoneNumber");
+            table.Columns.Add("Email");
+        }
+    }
+}
--- AddressBookLINQ/Program.cs
+++ AddressBookLINQ/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------------------Welcome To Address Book Linq------------------------------------");
+            AddressBookTable table = new AddressBookTable();
+            table.AddToDataTable();
+            Console.ReadKey();
+
+        }
+    }
+}
